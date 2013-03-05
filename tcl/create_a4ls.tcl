2eca87f12e2027bac129f7df66cbb6482c3315cf
Shepard Siegel <shepard.siegel@atomicrules.com>
2013-03-05 16:12:19 -0500

    hkp3 - Two a4ls, built and tested

--- tcl/create_a4ls.tcl
+++ tcl/create_a4ls.tcl
@@ -5,10 +5,24 @@
 ipx::infer_core {/home/shep/projects/hotline/ip/a4ls}
 
 set_property vendor {atomicrules.com} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 set_property library {hotline} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 set_property display_name {AR Hotline AXI4-Lite Slave} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 set_property vendor_display_name {Atomic Rules LLC} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 set_property company_url {www.atomicrules.com} [ipx::current_core]
+ipx::save_core [ipx::current_core]
+
+set_property range {4096} [ipx::get_address_block reg0 [ipx::get_memory_map s_axi [ipx::current_core]]]
+ipx::save_core [ipx::current_core]
+
+puts "This is the IP Packager 2013.x bugfix Scott and Amit want us to apply..."
+set_property value {s_axi_aresetn} [ipx::get_bus_parameter ASSOCIATED_RESET [ipx::get_bus_interface s_axi_signal_clock [ipx::current_core]]]
+ipx::save_core [ipx::current_core]
 
 set_property ip_repo_paths  /home/shep/projects/hotline/ip/a4ls [current_fileset]
-update_ip_catalog
+ipx::save_core [ipx::current_core]
+
+update_ip_catalog -rebuild
--- tcl/create_l2HCrt.tcl
+++ tcl/create_l2HCrt.tcl
@@ -5,19 +5,33 @@
 ipx::infer_core {/home/shep/projects/hotline/ip/l2HCrt}
 
 set_property vendor {atomicrules.com} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 set_property library {hotline} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 set_property display_name {AR Hotline Ethernet to AXI4-Lite Bridge} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 set_property vendor_display_name {Atomic Rules LLC} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 set_property company_url {www.atomicrules.com} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 
 ipx::remove_port {CLK_GATE_m_axi_aclk} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 ipx::remove_port {CLK_GATE_rxclk_ModBnd} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 ipx::remove_port {rxclk_ModBnd} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 ipx::remove_port {CLK_GATE_gmii_gtx_clk} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 ipx::remove_port {gmii_led} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 ipx::remove_port {gmii_int} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 
 ipx::infer_bus_interfaces {{xilinx.com:interface:gmii:1.0}} [ipx::current_core]
+ipx::save_core [ipx::current_core]
 
 set_property ip_repo_paths  {/home/shep/projects/hotline/ip/a4ls /home/shep/projects/hotline/ip/l2HCrt} [current_fileset]
-update_ip_catalog
+ipx::save_core [ipx::current_core]
+
+update_ip_catalog -rebuild
